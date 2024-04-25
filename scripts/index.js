d91d753d7e0375e7e3213772b2683e05eb4bbe6c
marinka-bobidi <155455955+marinka-bobidi@users.noreply.github.com>
2024-04-25 19:50:17 +0300

    Объвлен клон карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,13 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector("#card-template");
+
+// @todo: Функция создания карточки
+function createCard(cardData, removeCard) {
+  const cardClone = cardTemplate.content.cloneNode(true);
+  const cardTitle = cardClone.querySelector(".card__title");
+  const cardImage = cardClone.querySelector(".card__image");
+  const deleteButton = cardClone.querySelector(".card__delete-button");
+
+  cardTitle.textContent = cardData.name;
+  cardImage.src = cardData.link;
+}
