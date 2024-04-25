d501cc314888769fb6bfa40c4ce94e95cbac0228
marinka-bobidi <155455955+marinka-bobidi@users.noreply.github.com>
2024-04-25 19:38:57 +0300

    Создан тамплейт карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,2 @@
 // @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+const cardTemplate = document.querySelector("#card-template");
