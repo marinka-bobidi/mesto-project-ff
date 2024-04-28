6b203701a30277e1ea5748138bc74ebff064cf45
marinka-bobidi <155455955+marinka-bobidi@users.noreply.github.com>
2024-04-28 18:25:27 +0300

    Функция удаления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,19 @@ function createCard(cardData, removeCard) {
   const cardTitle = cardClone.querySelector(".card__title");
   const cardImage = cardClone.querySelector(".card__image");
   const deleteButton = cardClone.querySelector(".card__delete-button");
+  const cardElement = cardClone.querySelector(".card");
 
   cardTitle.textContent = cardData.name;
   cardImage.src = cardData.link;
+
+  deleteButton.addEventListener("click", function () {
+    removeCard(cardElement);
+  });
+
+  return cardClone;
+}
+
+// @todo: Функция удаления карточки
+function removeCard(cardElement) {
+  cardElement.remove();
 }
