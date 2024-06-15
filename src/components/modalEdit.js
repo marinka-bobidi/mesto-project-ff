90e77fbb59e088f53e5e8816029166f8c8d8ac79
marinka-bobidi <155455955+marinka-bobidi@users.noreply.github.com>
2024-06-15 20:19:38 +0300

    reset to sprint 6

--- src/components/modalEdit.js
+++ src/components/modalEdit.js
@@ -5,6 +5,7 @@ import {
   jobProfile,
   modalEdit,
 } from "../pages/index.js";
+
 import { closeModal } from "./modal.js";
 
 const formElement = document.querySelector(".popup__form");
@@ -13,9 +14,18 @@ function handleFormSubmit(evt) {
   evt.preventDefault();
 
   nameProfile.textContent = nameInputEdit.value;
+
   jobProfile.textContent = jobInputEdit.value;
 
   closeModal(modalEdit);
 }
 
+function onOpenCallback() {
+  nameInputEdit.value = nameProfile.textContent;
+
+  jobInputEdit.value = jobProfile.textContent;
+}
+
 formElement.addEventListener("submit", handleFormSubmit);
+
+export { onOpenCallback };
